7a9b3f9ed7f2c7b82fc6f061a676623e36f00682
David <dhack8@gmail.com>
2016-04-21 11:15:12 +1200

    Ran it through a compiler.
    Aside from obvious reasons that it wont compile (comments in if conditions), it compiles.
    When actually used will need all the includes and externs as well

--- pseudocode.c
+++ pseudocode.c
@@ -5,7 +5,7 @@ Just some pseudo code i (david) am putting togethet based on lectures and what i
 int main(){
   bool hasLine = true;
   int sum = 0;
-  int location = 0;
+  int locationLine;
   int center = 160;
   int range = 0;
   int error = 0;
@@ -15,16 +15,15 @@ int main(){
   int maxP = 0;
   int lineWidth = 10;//dont actully know what the line width will be but this value should be slightly larger then the line with in pixels
   while(true){//loops forever
-    while(hasline){//loops while robot has line
+    while(hasLine){//loops while robot has line
       sum = 0; //reset sum
-      location = 0; //reset location
       take_picture();
-      for(i = 0, i<320, i++){ // loop across image
+      for(int i = 0; i<320; i++){ // loop across image
         p = get_pixel(120,i,3); // p equals pixel
         if(p>240){ p = 255; } else { p = 0; } // flattens anything that isnt COMPLETElY white to 0 (black)
         if(p == 255){ //if pixel was white
           sum = sum + i; //add i location to sum
-          numfound++; //increment number of white pixels found
+          numFound++; //increment number of white pixels found
           if(i< minP){ //code to find the rangeof white pixels found
             minP = i;
           }else if(i>maxP){
@@ -33,20 +32,22 @@ int main(){
         }
       }//exit of for loop
       if(numFound != 0){
-        location = sum/numFound; // finds middle of white line
+        locationLine = sum/numFound; // finds middle of white line
+      }else{
+        locationLine = 0;
       }
       range = maxP -minP;
-      error = center - location;
-      if(location == 0 && /*left sensor has wall*/){ //code for entering the fourth quad
-        hasline = false;
+      error = center - locationLine;
+      if(locationLine == 0 && /*left sensor has wall*/){ //code for entering the fourth quad
+        hasLine = false;
         continue;
-      }else if(/*front sensor has object*/ && location != 0){ //if we have a ine but theres a wall
+      }else if(/*front sensor has object*/ && locationLine != 0){ //if we have a ine but theres a wall
         /*code for stopping the robot and opening the gate goes here*/
-      }else if(range>lineWidth && location < 180){ //this is either a T or 4 way junction or a 90 degree left turn (this might catch sharp left turns to)
+      }else if(range>lineWidth && locationLine < 180){ //this is either a T or 4 way junction or a 90 degree left turn (this might catch sharp left turns to)
         /*code for turning 90 degrees left*/
-      }else if(range>lineWidth && location >= 180){ //this is a 90 degree right turn
+      }else if(range>lineWidth && locationLine >= 180){ //this is a 90 degree right turn
         /*code for turning 90 degrees right*/
-      }else if(location == 0){ //lost line, no wall and no gate
+      }else if(locationLine == 0){ //lost line, no wall and no gate
         /*code for turning 180 degrees*/
       }else if(range<lineWidth){ //just an normal line
         if(error<-3){ //rightish turn
