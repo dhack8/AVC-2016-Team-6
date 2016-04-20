fc8c949ae07ee36ea7cc6ac04340befacc49e620
David <dhack8@gmail.com>
2016-04-20 18:29:04 +1200

    Started writing

--- pseudocode.c
+++ pseudocode.c
@@ -1,1 +1,12 @@
-
+/*
+Just some pseudo code i (david) am putting togethet based on lectures and what i think up in the shower :D
+*/
+//Includes and extern stuff here
+int main(){
+  boolean hasLine = true;
+  while(true){
+    while(hasline){
+      take_picture();
+    }
+  }
+}
