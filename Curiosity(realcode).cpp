f85525810527b6eee3cc836800148e17bbad9fa2
SanthuCheriyan <santhu.cherian@gmail.com>
2016-04-21 13:35:48 +1200

    Started framework for real code.
    Its quite different from the pseudo code but we can change it later. I was trying something but I don't know if it will work. My reason behind the change is that Davids code requires us knowing certain values instead of the robot figuring out the values through the picture which it needs to do.

--- Curiosity(realcode).cpp
+++ Curiosity(realcode).cpp
@@ -1,1 +1,57 @@
+//includes
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
 
+
+extern "C" int init(int d_lev);
+extern "C" int take_picture();
+extern "C" char get_pixel(int row, int col, int color);
+extern "C" void set_pixel(int col, int row, char red, char green, char blue);
+extern "C" set_motor(int motor, int speed);
+extern "C" Sleep(int sec, int usec);
+extern "C" read_analog(int ch_adc);
+extern "C" write_digital(int chan, int direct);
+extern "C" read_digital(int chan);
+
+int main(){
+  bool hasLine = true;
+  int sum = 0;
+  int error = 0;
+  int range = 0;
+  int pixelWidth = 0;
+  
+  int center = 160;
+  
+  
+  while(true){
+  
+  while(pixelWidth < 320){
+    int pixelHeight = 0;
+    while(pixelHeight < 240){
+      take_picture();
+      pixel = get_pixel(pixelWidth, pixelHeight);
+      pixelHeight++;
+      if(pixel){
+        
+        }
+      }
+    }
+    pixelWidth++;
+  }
+  }
+  
+  return 0;
+}
+
+int movement(int usec, int speed){
+  //will make the robot go forward
+  set_motor(1, 255);
+  set_motor(2, -255);
+  
+  return 0;
+}
+
+int imageProcessing(){
+  return 0;
+}
