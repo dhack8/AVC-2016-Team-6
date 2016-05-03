f0fd03739a7cc8c49ab4b2b3b912b62c2170d189
SanthuCheriyan <santhu.cherian@gmail.com>
2016-05-04 11:52:14 +1200

    Added the turning code
    very very early stages of the moving code

--- Curiosity(realcode).cpp
+++ Curiosity(realcode).cpp
@@ -2,17 +2,65 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <unistd.h>
+#include <pigpio.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <errno.h>
 
-
+extern "C" int init_hardware();
 extern "C" int init(int d_lev);
+
 extern "C" int take_picture();
 extern "C" char get_pixel(int row, int col, int color);
-extern "C" void set_pixel(int col, int row, char red, char green, char blue);
-extern "C" set_motor(int motor, int speed);
-extern "C" Sleep(int sec, int usec);
-extern "C" read_analog(int ch_adc);
-extern "C" write_digital(int chan, int direct);
-extern "C" read_digital(int chan);
+extern "C" void set_pixel(int col, int row, char red,char green,char blue);
+
+extern "C" int open_screen_stream();
+extern "C" int close_screen_stream();
+extern "C" int update_screen();
+//extern "C" void GetLine(int row,int threshold);
+extern "C" int display_picture(int delay_sec,int delay_usec);
+extern "C" int save_picture(char filename[5]);
+
+extern "C" int set_motor(int motor,int speed);
+
+extern "C" int read_analog(int ch_adc);
+extern "C" int Sleep(int sec, int usec);
+extern "C" int select_IO(int chan, int direct);
+extern "C" int write_digital(int chan,char level);
+
+extern "C" int read_digital(int chan);
+extern "C" int set_PWM(int chan, int value);
+
+extern "C" int connect_to_server( char server_addr[15],int port);
+extern "C" int send_to_server(char message[24]);
+extern "C" int receive_from_server(char message[24]);
+
+int main(){
+
+init(0);
+
+take_picture();
+display_picture(1, 0);
+
+set_motor(1, 127.5);
+set_motor(2, -127.5);
+Sleep(0, 500000);
+
+set_motor(1, 0);
+set_motor(2, 0);
+Sleep(1, 0);
+}
+
+
+
+
+
+
 
 int main(){
   bool hasLine = true;
@@ -26,28 +74,26 @@ int main(){
   
   while(true){
   
-  for(pixelWidth = 0; pixelWidth < 160; pixelWidth++){
-    for(pixelHeight = 0; pixelHeight < 120; pixelHeight++){
-      take_picture();
-      pixel = get_pixel(pixelWidth, pixelHeight, 3);
-        if (pixel>240){
-          pixel = 255
-        }else {
-          pixel = 0
+  for(pixelWidth = 0; pixelWidth < 320; pixelWidth++){
+    take_picture();
+    pixel = get_pixel(1, pixelWidth, 3);
+      if (pixel>240){
+        pixel = 255
+      }else {
+         pixel = 0
+       }
+       if (pixel = 255){
+        sum = sum + pixelHeight
+        numFound++;
+        if(pixelWidth< minP){ //code to find the rangeof white pixels found
+          minP = pixelWidth;
+        }else if(pixelWidth>maxP){
+           maxP = pixelWidth;
         }
-        if (pixel = 255){
-          sum = sum + pixelHeight
-          numFound++;
-          if(pixelWidth< minP){ //code to find the rangeof white pixels found
-            minP = pixelWidth;
-          }else if(pixelWidth>maxP){
-            maxP = pixelWidth;
-          }
-        }
-      }
-      pixelWidth++;
-    }
-  }
+       }
+     }
+   }
+  
   if(numFound != 0){
         locationLine = sum/numFound; // finds middle of white line
       }else{
@@ -59,14 +105,7 @@ int main(){
   return 0;
 }
 
-int movement(int usec, int speed){
-  //will make the robot go forward
-  set_motor(1, 255);
-  set_motor(2, -255);
-  
-  return 0;
-}
 
-int imageProcessing(){
-  return 0;
-}
+
+
+
