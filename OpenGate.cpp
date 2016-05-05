9a0c7229e6a7bf2ec0eb5714ab66df6bd70c8a4c
TimMBlyth <timmblyth@gmail.com>
2016-05-05 15:28:17 +1200

    Improve commenting/readability of OpenGate
    Moved comments onto the line that they apply to, rather than above. Added more comments to clarify what commands are doing to aid understanding by the rest of the group.

--- OpenGate.cpp
+++ OpenGate.cpp
@@ -1,21 +1,20 @@
+//Includes
 # include <stdio.h>
 # include <time.h>
 
+//Externs
 extern "C" int init(int d_lev);
 extern "C" int connect_to_server( char server_addr[15],int port);
 extern "C" int send_to_server(char message[24]);
 extern "C" int receive_from_server(char message[24]);
 
 int main (){
-   // This sets up the RPi hardware and ensures
-   // everything is working correctly
-   init(1);
-   //connects to server with the ip address 130.195.6.196, port 1024
-   connect_to_server("130.195.6.196", 1024);
-   //Asks the connected server for the password (nicely)
-   send_to_server("Please");
-   //receives the password from the connected server
-   char pass[24];
-   receive_from_server(pass);
-   send_to_server(pass);
+   
+   init(1);                                  //This sets up the RPi hardware and ensures everything is working correctly
+   
+   connect_to_server("130.195.6.196", 1024); //Connects to server with the ip address 130.195.6.196, port 1024
+   send_to_server("Please");                 //Asks the connected server for the password (nicely)
+   char pass[24];                            //Define the password variable
+   receive_from_server(pass);                //Receives the password from the connected server
+   send_to_server(pass);                     //Sends the password to the server
 return 0;}
