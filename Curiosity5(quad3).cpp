46d9d72102a32dd53efd9885993046032db91a84
SanthuCheriyan <santhu.cherian@gmail.com>
2016-05-23 13:25:16 +1200

    inital code for Quad 3
    Added all our initial code for Quad 3 we dont know if it will work but this is just concept

--- Curiosity5(quad3).cpp
+++ Curiosity5(quad3).cpp
@@ -44,9 +44,13 @@ int main(){
   init(0);
   int i = 0;
   int pixel = 0;
+  int pixel2 = 0;
   int sum;
+  int sum2;
   int numFound;
+  int numFound2;
   int locationLine;
+  int locationLine2;
   int error = 0;
   int center = 160;
   int P = 0;
@@ -55,12 +59,12 @@ int main(){
   int D = 0;
   float kD = 0.3;
   
-  connect_to_server("130.195.6.196", 1024); //Connects to server with the ip address 130.195.6.196, port 1024
-  send_to_server("Please");                 //Asks the connected server for the password (nicely)
-  char pass[24];                            //Define the password variable
-  receive_from_server(pass);                //Receives the password from the connected server
-  send_to_server(pass);                     //Sends the password to the server
-  Sleep(2,0);
+  //connect_to_server("130.195.6.196", 1024); //Connects to server with the ip address 130.195.6.196, port 1024
+  //send_to_server("Please");                 //Asks the connected server for the password (nicely)
+  //char pass[24];                            //Define the password variable
+  //receive_from_server(pass);                //Receives the password from the connected server
+  //send_to_server(pass);                     //Sends the password to the server
+  //Sleep(2,0);
   
   while(true){ //Continuos loop that goes forever
     take_picture(); //grab camera pic
@@ -69,15 +73,42 @@ int main(){
     locationLine = 0;
     for(i = 0; i < 320; i++){ //traverse along picture in middle
       pixel = get_pixel(i, 1, 3);
+      pixel2 = get_pixel(i, 20, 3)
       if (pixel>115){ //flattening finding white pixels
         sum = sum + i;
         numFound++;
       }
+      if(pixel2>115){
+        sum2 = sum2 + i
+        numFound2++;
+      }
+    }
+    if((sum > 12000 && sum < 13880) || sum > 50000){//90deg turn left
+      set_motor(1, 40);
+      set_motor(2, 40);
+      Sleep(1,0);
+      set_motor(1, -62);
+      set_motor(2, 62);
+      Sleep(0,500000);
+    }else if(sum > 38480 && sum <= 50000){
+      set_motor(1, 40);
+      set_motor(2, 40);
+      Sleep(1,0);
+      set_motor(1, 62);
+      set_motor(2, -62);
+      Sleep(0,500000);
     }
     if(numFound != 0){
       locationLine = sum/numFound; // finds middle of white line
     }
-    if(numFound < 20){ // if lost line (not enough white pixels for there to be a line)
+    if(numFound2 != 0){
+      locationLine2 = sum2/numFound2;
+    }
+    if(numFound < 20 && locationLine2 > 120 && locationLine2 < 200){
+      set_motor(1, 123);
+      set_motor(2, -123);
+      Sleep(0, 500000);
+    }else if(numFound < 20){ // if lost line (not enough white pixels for there to be a line)
       set_motor(1, -35); //reverse
       set_motor(2, -35);
       Sleep(0, 500000); //half a second
